9bef5470180f6c94044a93fb59f09cab5948bd4e
Nusrat Jahan <80273979+nusrat-mou@users.noreply.github.com>
2021-06-19 11:37:52 +0600

    Add files via upload

---
+++ Task3/process.php
@@ -0,0 +1,42 @@
+<?php include "control/myaction.php"; ?>
+
+<?php
+   	
+	   //Get form data
+	   $formdata = array(
+	      'First Name' => $_POST["fname"],
+	      'Email' => $_POST["email"],
+	      'Password' => $_POST["Password"],
+          'Comment' => $_POST['Comment'],
+          'Gender' => $_POST['gender'],
+           'Fileupload' => $target_file,
+		   'Hobby 1' => $_POST['Hobby1'],
+		   'Hobby 2' => $_POST['Hobby2'],
+		   'Hobby 3' => $_POST['Hobby3'],
+	   );
+       $existingdata = file_get_contents('data.json');
+       $tempJSONdata = json_decode($existingdata);
+       $tempJSONdata[] = $formdata;
+       //Convert updated array to JSON
+	   $jsondata = json_encode($tempJSONdata, JSON_PRETTY_PRINT);
+	   
+	   //write json data into data.json file
+	   if(file_put_contents("data.json", $jsondata)) {
+	        echo "Data successfully saved <br>";
+	    }
+	   else 
+	        echo "no data saved";
+
+     $data = file_get_contents("data.json");
+	 $mydata = json_decode($data);
+
+    
+	 foreach($mydata as $myobject)
+	 {
+        foreach($myobject as $key=>$value)
+        {
+            echo "Your ".$key." is: ".$value."<br>";
+        } 
+	}
+	
+?>
